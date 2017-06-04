3967ed34c4b5abcd7c046cfe43b72fdc3bde54e9
Mohamed <slimani.m1993@gmail.com>
2017-06-04 05:24:47 +0100

    change the layouts.app to sb-admin template style also the title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.app')
 
+@section('title')
+        Home
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Home</h1>
+        </div>
+        <!-- /.col-lg-12 -->
         <div class="col-md-8 col-md-offset-2">
+
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
@@ -12,6 +19,4 @@
                 </div>
             </div>
         </div>
-    </div>
-</div>
 @endsection
