1b375ef7ff8113a82620dc3dd139a6e8800e1d24
Mohamed <slimani.m1993@gmail.com>
2017-06-08 21:38:33 +0100

    Change the logging view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,106 +1,106 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Interact - Connexion</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="dist/css/sb-admin-2.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Interact | Connexion</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('dist/css/AdminLTE.min.css') }}">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="{{ asset('vendor/iCheck/square/blue.css') }}">
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 </head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="{{ asset('index2.html') }}"><b>Interact</b> UDL-SBA</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Connexion</p>
+    
+    <form role="form" method="POST" action="{{ route('login') }}">
+      
+      {{ csrf_field() }}
 
-<body>
+      <div class="form-group has-feedback">
+        <input type="email" class="form-control{{ $errors->has('email') ? ' has-error' : '' }}" placeholder="E-mail" name="email" type="email" value="{{ old('email') }}" required autofocus>
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        @if ($errors->has('email'))
+        <span class="help-block">
+            <strong>{{ $errors->first('email') }}</strong>
+        </span>
+        @endif
+      </div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="login-panel panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Please Sign In</h3>
-                    </div>
-                    <div class="panel-body">
-                        <form role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-                            <fieldset>
-                                <div class="form-group">
-                                    <input class="form-control{{ $errors->has('email') ? ' has-error' : '' }}" placeholder="E-mail" name="email" type="email" value="{{ old('email') }}" required autofocus>
-                                    @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control{{ $errors->has('password') ? ' has-error' : '' }}" placeholder="Password" name="password" type="password" required>
-                                    @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                                <div class="checkbox text-center">
-                                    <label>
-                                        <input name="remember" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>Remember Me
-                                    </label>
-                                </div>
-                                <div class="text-center">
-                                    <div class="btn-group">
-                                        <button type="submit" class="btn btn-primary">
-                                            Login
-                                        </button>
-                                        <button type="submit" class="btn btn-primary">
-                                            <i class="fa fa-facebook"></i> Login with facebook
-                                        </button>    
-                                    </div>
-                                </div>
-                                <div class="text-center">
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        Forgot Your Password?
-                                    </a>
-                                </div>
-                            </fieldset>
-                        </form>
-                    </div>
-                </div>
-            </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control{{ $errors->has('password') ? ' has-error' : '' }}" placeholder="Password" name="password" type="password" required>
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        @if ($errors->has('password'))
+        <span class="help-block">
+            <strong>{{ $errors->first('password') }}</strong>
+        </span>
+        @endif
+      </div>
+
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Restez Connecté
+            </label>
+          </div>
         </div>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Connexion</button>
+        </div>
+        <!-- /.col -->
+      </div>
+
+    </form>
+
+    <div class="social-auth-links text-center" data-toggle="social" data-placement="bottom" title="comming soon">
+      <p>- OR -</p>
+      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat disabled"><i class="fa fa-facebook"></i> Sign in using Facebook</a>
+      <a href="#" class="btn btn-block btn-social btn-google btn-flat disabled"><i class="fa fa-google-plus"></i> Sign in using Google+</a>
     </div>
+    <!-- /.social-auth-links -->
 
-    <!-- jQuery -->
-    <script src="vendor/jquery/jquery.min.js"></script>
+    <a href="{{ route('password.request') }}">Forgot Your Password?</a><br>
+    <a href="register.html" class="text-center">Register a new membership</a>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
 
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="dist/js/sb-admin-2.js"></script>
-
+<!-- jQuery 2.2.3 -->
+<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<!-- iCheck -->
+<script src="{{ asset('vendor/iCheck/icheck.min.js') }}"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' // optional
+    });
+    $('[data-toggle="social"]').tooltip();
+  });
+</script>
 </body>
-
 </html>
